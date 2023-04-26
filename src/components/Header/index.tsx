70d9752e7278027e1b1cd0c7bcb68a49be8a62b0
Felipe Oliveira <oliveira.felipe28@gmail.com>
2023-04-26 18:35:57 -0300

    feat: initial structure

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import "./index.css";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      <div className="menu-div">
+        <p>Menu</p>
+      </div>
+      <div className="user-name-div">
+        <div className="red-icon-div" />
+        <p>User Name</p>
+      </div>
+      <div className="sales-report-div">
+        <p>Sales Report</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
