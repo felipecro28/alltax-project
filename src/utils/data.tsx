d954a04f60d58a641328d420e8d0c74228fbd6dc
Felipe Oliveira <oliveira.felipe28@gmail.com>
2023-04-26 19:00:42 -0300

    feat: created data model

---
+++ src/utils/data.tsx
@@ -0,0 +1,301 @@
+export const productsArray = [
+  {
+    categoria: "Comida",
+    id: "cereais1",
+    produto: "Cereais",
+    marca: "Cereais 1",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 412,
+      },
+      {
+        mes: 2,
+        quantidade: 318,
+      },
+      {
+        mes: 3,
+        quantidade: 545,
+      },
+      {
+        mes: 4,
+        quantidade: 2,
+      },
+    ],
+  },
+  {
+    categoria: "Comida",
+    id: "cereais2",
+    produto: "Cereais",
+    marca: "Cereais 2",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 323,
+      },
+      {
+        mes: 2,
+        quantidade: 465,
+      },
+      {
+        mes: 3,
+        quantidade: 154,
+      },
+      {
+        mes: 4,
+        quantidade: 419,
+      },
+    ],
+    preco: 28,
+  },
+  {
+    categoria: "Comida",
+    id: "congelados1",
+    produto: "Congelados",
+    marca: "Congelados 1",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 249,
+      },
+      {
+        mes: 2,
+        quantidade: 522,
+      },
+      {
+        mes: 3,
+        quantidade: 342,
+      },
+      {
+        mes: 4,
+        quantidade: 442,
+      },
+    ],
+    preco: 47,
+  },
+  {
+    categoria: "Comida",
+    id: "congelados2",
+    produto: "Congelados",
+    marca: "Congelados 2",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 495,
+      },
+      {
+        mes: 2,
+        quantidade: 62,
+      },
+      {
+        mes: 3,
+        quantidade: 399,
+      },
+      {
+        mes: 4,
+        quantidade: 117,
+      },
+    ],
+    preco: 14,
+  },
+  {
+    categoria: "Bebida",
+    id: "sucos1",
+    produto: "Sucos",
+    marca: "Sucos 1",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 47,
+      },
+      {
+        mes: 2,
+        quantidade: 196,
+      },
+      {
+        mes: 3,
+        quantidade: 250,
+      },
+      {
+        mes: 4,
+        quantidade: 162,
+      },
+    ],
+    preco: 37,
+  },
+  {
+    categoria: "Bebida",
+    id: "sucos2",
+    produto: "Sucos",
+    marca: "Sucos 2",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 404,
+      },
+      {
+        mes: 2,
+        quantidade: 491,
+      },
+      {
+        mes: 3,
+        quantidade: 9,
+      },
+      {
+        mes: 4,
+        quantidade: 29,
+      },
+    ],
+    preco: 50,
+  },
+  {
+    categoria: "Bebida",
+    id: "refrigerantes1",
+    produto: "Refrigerantes",
+    marca: "Refrigerantes 1",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 556,
+      },
+      {
+        mes: 2,
+        quantidade: 141,
+      },
+      {
+        mes: 3,
+        quantidade: 137,
+      },
+      {
+        mes: 4,
+        quantidade: 253,
+      },
+    ],
+    preco: 31,
+  },
+  {
+    categoria: "Bebida",
+    id: "refrigerantes2",
+    produto: "Refrigerantes",
+    marca: "Refrigerantes 2",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 373,
+      },
+      {
+        mes: 2,
+        quantidade: 137,
+      },
+      {
+        mes: 3,
+        quantidade: 173,
+      },
+      {
+        mes: 4,
+        quantidade: 420,
+      },
+    ],
+    preco: 43,
+  },
+  {
+    categoria: "Têxtil",
+    id: "calcas1",
+    produto: "Calças",
+    marca: "Calças 1",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 9,
+      },
+      {
+        mes: 2,
+        quantidade: 547,
+      },
+      {
+        mes: 3,
+        quantidade: 568,
+      },
+      {
+        mes: 4,
+        quantidade: 204,
+      },
+    ],
+    preco: 50,
+  },
+  {
+    categoria: "Têxtil",
+    id: "calcas2",
+    produto: "Calças",
+    marca: "Calças 2",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 543,
+      },
+      {
+        mes: 2,
+        quantidade: 317,
+      },
+      {
+        mes: 3,
+        quantidade: 53,
+      },
+      {
+        mes: 4,
+        quantidade: 233,
+      },
+    ],
+    preco: 40,
+  },
+  {
+    categoria: "Têxtil",
+    id: "camisas1",
+    produto: "Camisas",
+    marca: "Camisas 1",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 27,
+      },
+      {
+        mes: 2,
+        quantidade: 479,
+      },
+      {
+        mes: 3,
+        quantidade: 378,
+      },
+      {
+        mes: 4,
+        quantidade: 89,
+      },
+    ],
+    preco: 49,
+  },
+  {
+    categoria: "Têxtil",
+    id: "camisas2",
+    produto: "Camisas",
+    marca: "Camisas 2",
+    vendas: [
+      {
+        mes: 1,
+        quantidade: 120,
+      },
+      {
+        mes: 2,
+        quantidade: 147,
+      },
+      {
+        mes: 3,
+        quantidade: 345,
+      },
+      {
+        mes: 4,
+        quantidade: 557,
+      },
+    ],
+    preco: 4,
+  },
+];
